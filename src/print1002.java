d7efad997b56f5be1bd0d72118a857c54e87a1e7
han-jisu0525 <hanjs1501@hansung.ac.kr>
2023-06-28 03:41:08 +0900

    기초1. 출력문

---
+++ src/print1002.java
@@ -0,0 +1,5 @@
+public class print1002 {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
---
+++ src/print1008.java
@@ -0,0 +1,5 @@
+public class print1008 {
+    public static void main(String[] args) {
+        System.out.println("┌┬┐\n├┼┤\n└┴┘");
+    }
+}
