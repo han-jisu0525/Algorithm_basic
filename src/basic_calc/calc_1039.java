228978cabe1ff7f7908a056c7493f387bfdcbc9a
han-jisu0525 <hanjs1501@hansung.ac.kr>
2023-07-03 14:47:02 +0900

    기초1. 형변환(나머지), 산술연산(일부)

---
+++ src/basic_calc/calc_1039.java
@@ -0,0 +1,22 @@
+package basic_calc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class calc_1039 {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String[] num = br.readLine().split(" ");
+//        long result = Long.parseLong(num[0]) + Long.parseLong(num[1]);
+//        System.out.println(result);
+
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextLong();
+        long y = sc.nextLong();
+
+        System.out.println(x+y);
+
+    }
+}
---
+++ src/basic_printtrans/PT_1036.java
@@ -0,0 +1,20 @@
+package basic_printtrans;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class PT_1036 {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int x = br.readLine().charAt(0);
+//
+//        System.out.println(x);
+
+        Scanner sc = new Scanner(System.in);
+        char xx = sc.next().charAt(0);
+
+        System.out.println((int) xx);
+    }
+}
