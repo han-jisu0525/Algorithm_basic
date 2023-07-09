b87a6e23d59e6b0d9d2df0394d577dd05a2b8c16
han-jisu0525 <hanjs1501@hansung.ac.kr>
2023-07-10 03:56:43 +0900

    기초2. 비트단위논리연산(나머지), 삼항연산, 종합

---
+++ src/basic_ternaryOperator/TO_1064.java
@@ -0,0 +1,15 @@
+package basic_ternaryOperator;
+
+import java.util.Scanner;
+
+public class TO_1064 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int z = scanner.nextInt();
+
+        System.out.println((x>y?x:y)>z?(x>y?x:y):z);
+    }
+}
