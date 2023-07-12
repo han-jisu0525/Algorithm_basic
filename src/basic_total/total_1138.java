d301b08eb6259b9fa3fab8f81058782542f09c58
han-jisu0525 <hanjs1501@hansung.ac.kr>
2023-07-12 15:39:49 +0900

    기초2. 종합(끝)

---
+++ src/basic_total/total_1138.java
@@ -0,0 +1,29 @@
+package basic_total;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class total_1138 {
+    public static void main(String[] args) throws IOException {
+//        Scanner sc = new Scanner(System.in);
+//
+//        int x = sc.nextInt();
+//
+//        if (x==1) System.out.println("0");
+//        else if (x==0) System.out.println("1");
+//        else System.out.println("Error!");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+
+        int ans = a == 1 ? 0 : 1;
+
+        bw.write(String.valueOf(ans));
+
+        br.close();
+        bw.close();
+
+    }
+}
