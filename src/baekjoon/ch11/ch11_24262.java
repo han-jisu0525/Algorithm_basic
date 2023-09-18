4e48a5eb6f1e6eba3b7c2d2ae7554884d5c9dd80
han-jisu0525 <hanjs1501@hansung.ac.kr>
2023-09-18 23:44:59 +0900

    11단계_시간복잡도

---
+++ src/baekjoon/ch11/ch11_24262.java
@@ -0,0 +1,10 @@
+package baekjoon.ch11;
+
+public class ch11_24262 {
+    public static void main(String[] args) {
+        // 시간복잡도 O(1)
+
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
---
+++ src/baekjoon/ch11/ch11_24263.java
@@ -0,0 +1,17 @@
+package baekjoon.ch11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ch11_24263 {
+    public static void main(String[] args) throws IOException {
+        // 시간복잡도 O(n)
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
