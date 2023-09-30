70b918060e53d8e3ff64feb49cdc0ba4ce77f75b
han-jisu0525 <hanjs1501@hansung.ac.kr>
2023-09-30 23:51:49 +0900

    14단계_집합과 맵

---
+++ src/baekjoon/ch14/ch14_10816.java
@@ -0,0 +1,75 @@
+package baekjoon.ch14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class ch14_10816 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < M; i++) {
+            int key = Integer.parseInt(st.nextToken());
+
+            // upperBound와 lowerBound의 차이 값을 구한다.
+            sb.append(upperBound(arr, key) - lowerBound(arr, key)).append(' ');
+        }
+        System.out.println(sb);
+    }
+    private static int lowerBound(int[] arr, int key) {
+        int lo = 0;
+        int hi = arr.length;
+
+        // lo가 hi랑 같아질 때 까지 반복
+        while (lo < hi) {
+
+            int mid = (lo + hi) / 2;
+
+            //key 값이 중간 위치의 값보다 작거나 같을 경우
+            if (key <= arr[mid]) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+
+        }
+
+        return lo;
+    }
+
+    private static int upperBound(int[] arr, int key) {
+        int lo = 0;
+        int hi = arr.length;
+
+        // lo가 hi랑 같아질 때 까지 반복
+        while (lo < hi) {
+
+            int mid = (lo + hi) / 2;
+
+            // key값이 중간 위치의 값보다 작을 경우
+            if (key < arr[mid]) {
+                hi = mid;
+            } else { // 중복원소의 경우 else에서 처리된다.
+                lo = mid + 1;
+            }
+
+        }
+        return lo;
+    }
+}
